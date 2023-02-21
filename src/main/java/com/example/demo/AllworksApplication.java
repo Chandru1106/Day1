c9fdcd5e321fe5c9fe594c8d3e001561f1ba3032
Chandru1106 <727721euit022@skcet.ac.in>
2023-02-21 12:11:50 +0530

    First commit

---
+++ src/main/java/com/example/demo/AllworksApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllworksApplication.class, args);
+	}
+
+}
